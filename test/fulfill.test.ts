913ac7fe20064402eba6021c3da6b0ab0b48fc8f
P. Kevin Contreras <kevin.macwhinnie@me.com>
2024-07-11 22:25:39 -0400

    Finish fulfill tests for all branches

--- test/fulfill.test.ts
+++ test/fulfill.test.ts
@@ -18,41 +18,79 @@
  */
 
 import { describe, expect, it, jest } from "@jest/globals";
-import { FetchFunction, fulfill, NoAuthority, SereneAuthorityRefreshOptions, SereneRequest, SereneRequestParseOptions, SereneRequestPrepareOptions } from "../lib";
+import { FetchFunction, fulfill, SereneAuthority, SereneRequest, SereneRequestParseOptions, SereneRequestPrepareOptions } from "../lib";
 
 describe("fulfill module", () => {
     describe("#fulfill", () => {
         it("should refresh authority if invalid", async () => {
-            const authority = new MockAuthority();
-            expect(authority.isValid).toStrictEqual(false);
+            let isValid = false;
+            const refresh = jest.fn(async () => { isValid = true });
+            const authenticate = jest.fn(async () => { });
+            const authority: SereneAuthority = {
+                retryLimit: 1,
+                get isValid(): boolean {
+                    return isValid;
+                },
+                refresh,
+                authenticate,
+            };
 
             const request = new MockRequest();
             const fetch = jest.fn<FetchFunction>(async (_request) => new Response("{}"));
             await fulfill({ authority, request, fetch });
 
-            expect(authority.isValid).toStrictEqual(true);
+            expect(refresh.mock.calls).toHaveLength(1);
+            expect(isValid).toStrictEqual(true);
+        });
+
+        it("should refresh authority if 401 is returned", async () => {
+            const refresh = jest.fn(async () => { });
+            const authenticate = jest.fn(async () => { });
+            const authority: SereneAuthority = {
+                retryLimit: 1,
+                isValid: true,
+                refresh,
+                authenticate,
+            };
+
+            const request = new MockRequest();
+            let isFirstFetchCall = true;
+            const fetch = jest.fn<FetchFunction>(async (_request) => {
+                if (isFirstFetchCall) {
+                    isFirstFetchCall = false;
+                    return new Response("{}", { status: 401 });
+                } else {
+                    return new Response("{}");
+                }
+            });
+            await fulfill({ authority, request, fetch });
+
+            expect(refresh.mock.calls).toHaveLength(1);
+        });
+
+        it("should throw an error if all retries are exhausted", () => {
+            const refresh = jest.fn(async () => { });
+            const authenticate = jest.fn(async () => { });
+            const authority: SereneAuthority = {
+                retryLimit: 1,
+                isValid: false,
+                refresh,
+                authenticate,
+            };
+
+            const request = new MockRequest();
+            const fetch = jest.fn<FetchFunction>(async (_request) => new Response("{}", { status: 401 }));
+            expect(fulfill({ authority, request, fetch })).rejects.toThrow();
         });
     });
 });
 
-class MockAuthority extends NoAuthority {
-    private _isValid: boolean = false;
-
-    override get isValid(): boolean {
-        return this._isValid;
-    }
-
-    override async refresh(_options: SereneAuthorityRefreshOptions): Promise<void> {
-        this._isValid = true;
-    }
-}
-
-class MockRequest implements SereneRequest<MockAuthority, object> {
-    prepare(_options: SereneRequestPrepareOptions<MockAuthority>): Request {
+class MockRequest implements SereneRequest<SereneAuthority, object> {
+    prepare(_options: SereneRequestPrepareOptions<SereneAuthority>): Request {
         return new Request("about:blank");
     }
 
-    async parse({ fetchResponse }: SereneRequestParseOptions<MockAuthority>): Promise<object> {
+    async parse({ fetchResponse }: SereneRequestParseOptions<SereneAuthority>): Promise<object> {
         return await fetchResponse.json();
     }
 }
